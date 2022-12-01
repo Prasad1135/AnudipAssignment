a5e5998c1e839ef948d810244cff47e4aa327168
Prasad <Prasad@DESKTOP-UHJ2VOB>
2022-12-01 13:39:47 +0530

    1 Dec 2022

--- Assignment7/src/prasad/File.java
+++ Assignment7/src/prasad/File.java
@@ -1,40 +1,44 @@
 package prasad;
+
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Scanner;
+
 public class File {
+
+	public File(String string) {
+		// TODO Auto-generated constructor stub
+	}
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		File obj1=new File("C:\\New folder\\Demo file.txt");
-		Scanner sc = new Scanner(obj1);
+		File obj1 = new File("C:\\New folder\\Demo file.txt");
+		Scanner sc = new Scanner((Readable) obj1);
 		int counter = 0;
-		while(sc.hasNextLine()) 
-		{
+		while (sc.hasNextLine()) {
 			// count words
-					String str1 = sc.nextLine();
-					String[]arr = str1.split(" ");
-					// 
-					/*if(str1.length()>5) 
-					{
-						
-						System.out.println(" ");
-					}*/
-					counter +=arr.length;
+			String str1 = sc.nextLine();
+			String[] arr = str1.split(" ");
+			
+			  if(str1.length()>5) {
+			  
+			  System.out.println(" "); }
+			 
+			counter += arr.length;
 		}
-				System.out.println("count words:");
-				System.out.println(counter);
+		System.out.println("count words:");
+		System.out.println(counter);
 		FileInputStream obj = new FileInputStream("C:\\New folder\\Demo file.txt");
-		
+
 		int i;
 		String str = "";
-		
-		while((i=obj.read())!=-1)   // read data from text file
+
+		while ((i = obj.read()) != -1) // read data from text file
 		{
-			System.out.print((char)i);
+			System.out.print((char) i);
 		}
-		
-		obj.close();	
+
+		obj.close();
 	}
 
 }
